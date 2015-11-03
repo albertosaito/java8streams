85c07fa12706787ee0cca1d49336c6f0e997bb94
asaito <asaito@24hourfit.com>
2015-11-02 23:56:48 -0800

    NewsContent Stream Example

--- src/main/java/alberto/examples/streams/rsstext/NewsTextContent.java
+++ src/main/java/alberto/examples/streams/rsstext/NewsTextContent.java
@@ -1,4 +1,4 @@
-package alberto.examples.streams.mapreduce;
+package alberto.examples.streams.rsstext;
 
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
@@ -6,19 +6,23 @@ import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 
+import org.apache.commons.lang3.time.StopWatch;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.IOException;
 
 import java.net.URL;
 
 /**
- * MapReduce
+ * NewsTextContent
  *
  * @author <a href="mailto:nobuji.saito@gmail.com">nobuji.saito</a>
  * @version $Id$
  * @since Nov 2, 2015
  *
  */
-public class MapReduce {
+public class NewsTextContent {
 
     /**
      * Represents main
@@ -31,23 +35,38 @@ public class MapReduce {
      *
      */
     public static void main(final String args[]) throws IllegalArgumentException, FeedException, IOException {
+	final StopWatch stopwatch = new StopWatch();
+	stopwatch.start();
 	final URL feedSource = new URL("http://www.chicagotribune.com/news/local/breaking/rss2.0.xml");
 	final SyndFeedInput input = new SyndFeedInput();
 	// Create a SyndFeed object using an XMLReader object with the RSS feed
 	// source URL
 	final SyndFeed feed = input.build(new XmlReader(feedSource));
-	final String string = "chicago";
-
-	final int ocurrences = feed.getEntries().stream()
-		.map(entry -> checkIfStringPresentInTitle(entry, string))
-		.reduce(0, (a, b) -> a + b); // adds all values
-
-	System.out.printf("Total news with %s in the title is %d", string, ocurrences);
+	feed.getEntries().stream()
+	.map(entry -> {
+	    try {
+		return getNewsTextContent(entry);
+	    }
+	    catch(final IOException e) {
+		return "Unable to obtain text";
+	    }
+	})
+	.forEach(System.out::println);
+	stopwatch.stop();
+	System.out.printf("Elapsed time %f seconds", stopwatch.getNanoTime()/1000000000f);
     }
 
-    private static int checkIfStringPresentInTitle(final SyndEntry entry, final String string) {
-	final String title = entry.getTitle().toLowerCase();
-	return title.indexOf(string.toLowerCase()) > 0 ? 1 : 0;
+    /**
+     * Represents getNewsTextContent
+     *
+     * @param entry
+     * @return
+     * @throws IOException
+     * @since Nov 2, 2015
+     *
+     */
+    private static String getNewsTextContent(final SyndEntry entry) throws IOException {
+	final Document doc = Jsoup.connect(entry.getUri()).get();
+	return doc.text();
     }
-
 }
